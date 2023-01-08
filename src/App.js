462d6580804e6e41c553ef8bb9a1aa784f6169ba
mostafarmstu <mostafarmstu@gmail.com>
2023-01-08 20:08:11 +0600

    doc initialize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import MaterialTable from 'material-table'
-import XLSX from 'xlsx'
-import PrintIcon from '@material-ui/icons/Print'
-import GetAppOutlinedIcon from '@material-ui/icons/GetAppOutlined';
-import jsPDF from 'jspdf'
-import 'jspdf-autotable'
+import React from "react";
+import "./App.css";
+import MaterialTable from "material-table";
+import XLSX from "xlsx";
+import PrintIcon from "@material-ui/icons/Print";
+import GetAppOutlinedIcon from "@material-ui/icons/GetAppOutlined";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 const studentData = [
   {
     id: 1,
@@ -182,64 +182,76 @@ const studentData = [
     email: "rahul@gmail.com",
     year: 2020,
     fee: 784596,
-  }
-]
+  },
+];
 function App() {
   const columns = [
-    { title: "Name", field: "name", },
-    { title: "Email", field: "email", },
+    { title: "Name", field: "name" },
+    { title: "Email", field: "email" },
     { title: "Year", field: "year", type: "numeric" },
-    { title: "Fee", field: 'fee', type: "currency" }]
+    { title: "Fee", field: "fee", type: "currency" },
+  ];
 
   const downloadExcel = () => {
-    const newData = studentData.map(row => {
-      delete row.tableData
-      return row
-    })
-    const workSheet = XLSX.utils.json_to_sheet(newData)
-    const workBook = XLSX.utils.book_new()
-    XLSX.utils.book_append_sheet(workBook, workSheet, "students")
+    const newData = studentData.map((row) => {
+      delete row.tableData;
+      return row;
+    });
+    const workSheet = XLSX.utils.json_to_sheet(newData);
+    const workBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, "students");
     //Buffer
-    XLSX.write(workBook, { bookType: "xlsx", type: "buffer" })
+    XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
     //Binary string
-    XLSX.write(workBook, { bookType: "xlsx", type: "binary" })
+    XLSX.write(workBook, { bookType: "xlsx", type: "binary" });
     //Download
-    XLSX.writeFile(workBook, "ReportSheet.xlsx")
+    XLSX.writeFile(workBook, "ReportSheet.xlsx");
+  };
 
-
-  }
   const downloadPdf = () => {
-    const doc = new jsPDF()
-    doc.text("Student Details", 20, 10)
+    const doc = new jsPDF();
+    doc.text("Student Details", 20, 10);
     doc.autoTable({
       theme: "grid",
-      columns: columns.map(col => ({ ...col, dataKey: col.field })),
-      body: studentData
-    })
-    doc.save('ReportTable.pdf')
-  }
+      columns: columns.map((col) => ({ ...col, dataKey: col.field })),
+      body: studentData,
+    });
+    doc.save("ReportTable.pdf");
+  };
+
+  // const dowloadDoc = () => {
+  //   const doc = new 
+  // };
 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Export Data to Pdf in Material Table</h4>
+      <h4 align="center">Export Data to Pdf in Material Table</h4>
       <MaterialTable
         title="Student Details"
         columns={columns}
         data={studentData}
         actions={[
           {
-            icon: () => (<button>Export as Excel<GetAppOutlinedIcon /></button>),
+            icon: () => (
+              <button style={{ cursor: "pointer" }}>
+                Export Excel <GetAppOutlinedIcon />
+              </button>
+            ),
             tooltip: "Export to Excel",
             onClick: () => downloadExcel(),
-            isFreeAction: true
+            isFreeAction: true,
           },
           {
-            icon: () => (<button>Export as PDF<PrintIcon /></button>),
+            icon: () => (
+              <button style={{ cursor: "pointer" }}>
+                Export PDF <PrintIcon />
+              </button>
+            ),
             tooltip: "Export to Pdf",
             onClick: () => downloadPdf(),
-            isFreeAction: true
-          }
+            isFreeAction: true,
+          },
         ]}
       />
     </div>
